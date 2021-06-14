dc35b47464278bce23953652421dfd6fc0a620d0
steve erik <shenderson5454@gmail.com>
2021-06-13 21:03:22 -0400

    working on filtering

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const routes = require('./routes/apis');
+const cors = require('cors')
 const port = 3000
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use('/', routes);
+
 app.listen(port, () => console.log(`Running the best code on port: ${port}.`));
